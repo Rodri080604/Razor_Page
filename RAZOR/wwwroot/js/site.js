6c6e497c6de840e0afb05e944bfe3621b96fe323
Rodri080604 <huarachirodrigo76@gmail.com>
2025-08-15 21:09:39 -0400

    Primer avance del proyecto 1

--- RAZOR/wwwroot/js/site.js
+++ RAZOR/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const content = document.getElementById("content");
+    const toggleBtn = document.getElementById("sidebarToggle");
+
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapsed");
+    });
+});
+
