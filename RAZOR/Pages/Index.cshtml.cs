f4a5d63f1d37b482fc401bbd6121a6fc87253d42
Rodri080604 <huarachirodrigo76@gmail.com>
2025-08-19 21:11:23 -0400

    Integrando funciones al proyecto

--- RAZOR/Pages/Index.cshtml.cs
+++ RAZOR/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using RAZOR.Models;
 
 namespace RAZOR.Pages
 {
@@ -11,10 +15,20 @@ namespace RAZOR.Pages
         {
             _logger = logger;
         }
-
+        public List<Tarea> Tareas { get; set; } = new();
         public void OnGet()
         {
+            // Ruta del archivo JSON
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "tareas.json");
 
+            if (System.IO.File.Exists(filePath))
+            {
+                var json = System.IO.File.ReadAllText(filePath);
+
+                // Deserializar a Lista<Tarea>
+                Tareas = JsonSerializer.Deserialize<List<Tarea>>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
         }
     }
 }
---
+++ RAZOR/Pages/Tareas_Canceladas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RAZOR.Pages
+{
+    public class Tareas_CanceladasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
